fe8ac6db52ebe8151c583cd8b444a8669849fd17
Egor Ivankov <egor.ivankov2015@bk.ru>
2023-06-02 18:46:33 +0300

    the choice of a random character is made

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../Components/Header/Header";
-import HeroBlock from "../Components/Hero-block/Hero-block";
+import HeroBlockDescr from "../Components/Hero-block-descr/Hero-block-descr";
 import HeroCards from "../Components/Hero-cards/Hero-cards";
 import HeroInfo from "../Components/Hero-info/Hero-info";
 import vision from "../img/vision.png";
@@ -9,7 +9,7 @@ function App() {
     return (
         <div className="App">
             <Header/>
-            <HeroBlock/>
+            <HeroBlockDescr/>
             <div className="hero-content">
                 <HeroCards/>
                 <HeroInfo/>
--- src/Components/Hero-block-descr/Hero-block-descr.jsx
+++ src/Components/Hero-block-descr/Hero-block-descr.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "../../styles/style.scss";
 import MarvelService from "../services/MarvelServise";
+import mjolnir from "../../img/mjolnir.png";
 import Spinner from "../Spinner/Spinner";
 import ErrorMessage from "../Error-message/Error-message";
 
@@ -14,6 +15,10 @@ export default class HeroBlockDescr extends Component {
 
     componentDidMount() {
         this.updateHero();
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerId);
     }
 
     marvelService = new MarvelService();
@@ -40,18 +45,30 @@ export default class HeroBlockDescr extends Component {
             .catch(this.onError)
     }
 
+
     
     render() {
         const {hero, loading, error} = this.state;
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? <View hero={hero}/> : null;
-        
+
         return (
-            <div className='hero-block'>
+            <div className='hero-block-container'>
                 {errorMessage} 
                 {spinner}
                 {content}
+                <div className='hero-block-static'>
+                <p>
+                    Random character for today! <br/>
+                    Do you want to get to know him better?
+                </p>
+                <p className='hero-block-static-letter'>
+                    Or choose another one
+                </p>
+                <button onClick={this.updateHero} className='button-main'> Try it</button>
+                <img src={mjolnir} alt="Mjolnir"/>
+                </div>
             </div>
         )
     }
@@ -71,14 +88,16 @@ const View = ({hero}) => {
     const checkedDescription = checkDescr(description);
 
     return (
-        <div>
-            <img src={thumbnail} alt="Heroes"/>
-            <div className="hero-block-info">
-                <p className='hero-block-name'>{name}</p>
-                <p className='hero-block-descr'>{checkedDescription}</p>
-                <div className="hero-block-btns">
-                    <a href={homepage}><div className='button-main'> Homepage</div></a>
-                    <a href={wiki}><div className='button-secondary'>Wiki</div></a>
+        <div className="hero-block-container">
+            <div>
+                <img src={thumbnail} alt="Heroes" className="thumbnail"/>
+                <div className="hero-block-info">
+                    <p className='hero-block-name'>{name}</p>
+                    <p className='hero-block-descr'>{checkedDescription}</p>
+                    <div className="hero-block-btns">
+                        <a href={homepage}><div className='button-main'> Homepage</div></a>
+                        <a href={wiki}><div className='button-secondary'>Wiki</div></a>
+                    </div>
                 </div>
             </div>
         </div>

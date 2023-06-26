94a95169e4fe58861a7bfbe4b866bc45576fc22f
Egor-Ivankov <122007814+Egor-Ivankov@users.noreply.github.com>
2023-06-26 20:02:03 +0300

    App component rework

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import Header from "../Components/Header/Header";
 import HeroBlockDescr from "../Components/Hero-block-descr/Hero-block-descr";
 import HeroCards from "../Components/Hero-cards/Hero-cards";
@@ -6,19 +6,14 @@ import HeroInfo from "../Components/Hero-info/Hero-info";
 import ErrorBoundary from "../Components/Error-boundary/ErrorBoundary";
 import vision from "../img/vision.png";
 
-class App extends Component {
+function App () {
 
-    state = {
-        selectedHero: null,
+    const [selectedHero, setSelectedHero] = useState(null);
+
+    const onHeroSelected = (id) => {
+        setSelectedHero(id);
     }
 
-    onHeroSelected = (id) => {
-        this.setState({
-            selectedHero: id,
-        })
-    }
-
-    render() {
         return (
             <div className="App">
                 <Header/>
@@ -26,15 +21,14 @@ class App extends Component {
                     <HeroBlockDescr/>
                 </ErrorBoundary>
                 <div className="hero-content">
-                        <HeroCards onHeroSelected={this.onHeroSelected} />
+                        <HeroCards onHeroSelected={onHeroSelected} />
                         <ErrorBoundary>
-                            <HeroInfo heroId ={this.state.selectedHero}/>
+                            <HeroInfo heroId ={selectedHero}/>
                         </ErrorBoundary>
                 </div>
                 <img src={vision} alt="Vision" className="bg-decoration" />
             </div>
         );
-    }
 }
 
 export default App;

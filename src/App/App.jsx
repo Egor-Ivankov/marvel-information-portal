027058e9886ef5560103e3b65cef06bc447ca9df
Egor-Ivankov <122007814+Egor-Ivankov@users.noreply.github.com>
2023-06-30 12:23:14 +0300

    added page404

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from '../Components/Header/Header';
-import { MainPage, ComicsPage } from "../Components/pages";
-export default function App () {
+import { MainPage, ComicsPage, Page404 } from "../Components/pages";
+export default function App () {    
 
     return (
         <Router>
             <div className="App">
-                <Header/>
+            <Header/>
                 <main>
                     <Routes>
-                        <Route path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
+                        <Route path="/" element={<MainPage />}/>
+                        <Route path="/comics" element={<ComicsPage />}/>
+                        <Route path="*" element={<Page404 />}/>
                     </Routes>
                 </main>
             </div>
         </Router>
     );
 }
-
---
+++ src/Components/pages/404.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import background from '../../img/backgroundNoMatch.png';
+
+const Page404 = () => {
+    return (
+        <div>
+            <img className='background' src={background} alt="background" />
+            <div className='no-match-text'>
+                <h2>404 PAGE NOT FOUND</h2>
+                <strong>Protocol mising... Exiting program...</strong>
+                <p>
+                    Check that you typed the address correctly,
+                    <br/> go back to your previous page or try using our site 
+                    <br/>search to find something specific.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Page404;
